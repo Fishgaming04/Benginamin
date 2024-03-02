ab22ebcda5e2cfbd5b3c1a9b83d39434f7067f26
Fishgaming04 <pluvinage.raphael@gmail.com>
2024-03-02 21:06:20 +0100

    Update Transform

--- Minigin/Transform.cpp
+++ Minigin/Transform.cpp
@@ -1,14 +1,15 @@
 #include "Transform.h"
 
-void dae::Transform::SetLocalPosition(const float x, const float y, const float z)
-{
-	m_Localposition.x = x;
-	m_Localposition.y = y;
-	m_Localposition.z = z;
+void dae::Transform::SetLocalPosition(const float x, const float y, const float z){
+	SetLocalPosition(glm::vec3(x, y, z));
 }
-void dae::Transform::SetWorldPosition(const float x, const float y, const float z)
-{
-	m_Localposition.x = x;
-	m_Localposition.y = y;
-	m_Localposition.z = z;
+void dae::Transform::SetWorldPosition(const float x, const float y, const float z){
+	SetWorldPosition(glm::vec3(x, y, z));
 }
+
+void dae::Transform::SetLocalPosition(glm::vec3 Localposition) {
+	m_Localposition = Localposition;
+}
+void dae::Transform::SetWorldPosition(glm::vec3 Worldposition) {
+	m_Worldposition = Worldposition;
+} 
--- Minigin/Transform.h
+++ Minigin/Transform.h
@@ -9,7 +9,9 @@ namespace dae
 		const glm::vec3& getLocalposition() const { return m_Localposition; }
 		const glm::vec3& getWorldposition() const { return m_Worldposition; }
 		void SetLocalPosition(float x, float y, float z);
-		void SetWorldPosition(float x, float y, float z);
+		void SetWorldPosition(float x, float y, float z);		\
+		void SetLocalPosition(glm::vec3 Localposition);
+		void SetWorldPosition(glm::vec3 Worldposition);
 	private:	
 		glm::vec3 m_Localposition;
 		glm::vec3 m_Worldposition;
